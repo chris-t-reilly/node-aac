1b05bcc5c223854293c689e1d85b86b7e313c365
Chris Reilly <chris.reilly@playtech.com>
2016-08-25 15:00:41 +0200

    chore(test): update tests for new function parameters

--- test/decoder.js
+++ test/decoder.js
@@ -3,9 +3,6 @@ var path = require('path')
 
 var d = new Decoder()
 
-d.decode({
-	src: path.join(process.cwd(), 'test/fixtures/piano.aac'),
-	dest: 'piano-out.wav'
-}, function() {
+d.decode(path.join(process.cwd(), 'test/fixtures/piano.aac'), 'piano-out.wav', function() {
 	console.log('Done')
 })
--- test/encoder.js
+++ test/encoder.js
@@ -3,9 +3,6 @@ var path = require('path')
 
 var e = new Encoder()
 
-e.encode({
-	src: path.join(process.cwd(), 'test/fixtures/piano.wav'),
-	dest: 'piano-out.aac'
-}, function() {
+e.encode(path.join(process.cwd(), 'test/fixtures/piano.wav'), 'piano-out.aac', function() {
 	console.log('Done')
 })
